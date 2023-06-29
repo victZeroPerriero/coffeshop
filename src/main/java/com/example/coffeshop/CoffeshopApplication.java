2eb2deadafc04771f1067962d3ba2beea55df88c
victor <victzeran@mail.ru>
2023-06-29 15:43:55 +0300

    commit one

---
+++ src/main/java/com/example/coffeshop/CoffeshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeshopApplication.class, args);
+    }
+
+}
