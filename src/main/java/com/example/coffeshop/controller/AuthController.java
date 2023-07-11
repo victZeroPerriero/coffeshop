41767a699b490ca8ef2b07dea0e4250967f4f193
victor <victzeran@mail.ru>
2023-07-11 19:21:02 +0300

    commit dev creat auth

---
+++ src/main/java/com/example/coffeshop/controller/AuthController.java
@@ -0,0 +1,9 @@
+package com.example.coffeshop.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/api/auth")
+public class AuthController {
+}
